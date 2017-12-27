bbb0dca8ed36bbb7757a96b17019ccf161b1a979
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2017-12-27 14:34:10 +0530

    created simple tabledata

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
 
 import { JobService } from '../../services/job.service';
+import { Ijob } from '../../interfaces/Ijob';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
+
+displayedColumns: Array<string> = ['id', 'title', 'description', 'experience', 'no_of_vacancies' , 'skills'];
+dataSource = new DashBoardDataSource(this._jobService);
 
   constructor(private _jobService: JobService) { }
 
-  ngOnInit() {
-    this._getJobs();
-  }
+  ngOnInit() {}
+}
 
-  private _getJobs(): void {
-    this._jobService.getJobs()
-    .subscribe(
-      data => {
-        console.log(data);
-      }
-    );
+export class DashBoardDataSource extends DataSource<any> {
+  constructor(private _jobService: JobService) {
+    super();
   }
+  connect(): Observable<Ijob[]> {
+    return this._jobService.getJobs();
+  }
+  disconnect() {}
 }
