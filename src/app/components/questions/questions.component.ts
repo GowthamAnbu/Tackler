2a244e3c339f7858b65fd2ec105119061ab7c985
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2017-12-29 17:31:09 +0530

    trimmed questions-component

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Iquestion, Iquestions } from '../../interfaces/iquestion';
+
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css']
 })
+
 export class QuestionsComponent implements OnInit {
 
 private _testTaken = false;
@@ -20,6 +22,7 @@ finalToggle = false;
     this._getQuestions();
   }
 
+  /** returns the initial question object  */
   private _getQuestions() {
     this.questions = this._activatedRoute.snapshot.data['roundQuestions']; console.log(this.questions);
   }
@@ -28,6 +31,9 @@ finalToggle = false;
     return this._testTaken;
   }
 
+  /**
+   * initial function called by button click Take Test
+   * toggles testTaken for UI */
   takeTest(): void {
     this._initialHit();
     this._testTaken = !this._testTaken;
@@ -38,15 +44,15 @@ finalToggle = false;
     console.log('intial timer Hit');
   }
 
+  /** sets the liveQuestion based on the index given */
   private _setLiveQuestion(indexValue: number) {
     this.liveQuestion = {
       index: indexValue,
       question: this.questions.questions[indexValue]
-    }; console.log(this.liveQuestion);
-    /* this.liveQuestion.index = indexValue;
-    this.liveQuestion.question = this.questions.questions[indexValue]; */
+    };
   }
 
+  /** master Toggle controlling the liveQuestion to be viewed or not */
   toggle(index: number): void {
     index += 1;
     if (index === this.questions.questions.length) {
@@ -57,10 +63,12 @@ finalToggle = false;
     this._setLiveQuestion(index);
   }
 
+  /** things to do before hitting the api or service call  */
   finalPreHit(): void {
     this._postData();
   }
 
+  /** actual api hit or service call  */
   private _postData(): void {
     console.log('post api hit');
   }
