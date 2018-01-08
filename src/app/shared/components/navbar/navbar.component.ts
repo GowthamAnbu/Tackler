c2487b3b10133dcbbc8defe3f15c527c15958c7e
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2018-01-08 14:13:48 +0530

    updated navbar created authGuard UNCLEAN

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar} from '@angular/material';
 
 import { AuthService } from '../../../shared/services/auth.service';
 
@@ -9,9 +11,20 @@ import { AuthService } from '../../../shared/services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(public auth: AuthService ) { }
+  constructor(
+    public snackBar: MatSnackBar,
+    public authService: AuthService,
+    private _router: Router ) { }
 
   ngOnInit() {
+  }
+
+  public logout(): void {
+    this.authService.logout();
+    this._router.navigateByUrl('/login');
+    this.snackBar.open('You have Successfully logged out', 'success', {
+      duration: 3000,
+    });
   }
 
 }
